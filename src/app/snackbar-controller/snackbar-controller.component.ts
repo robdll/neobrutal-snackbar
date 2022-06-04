c4d4d8983a54bdad050cbc3225a1dea7dd6a97df
robdll <dilillo.roberto@gmail.com>
2022-06-04 18:59:11 +0100

    adds snackbar controller

--- src/app/snackbar-controller/snackbar-controller.component.ts
+++ src/app/snackbar-controller/snackbar-controller.component.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { SnackNotification } from '../models/notification.model';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
-  selector: 'snackbar-controller',
-  templateUrl: './snackbar-controller.component.html',
-  styleUrls: ['./snackbar-controller.component.scss']
+  selector: "snackbar-controller",
+  templateUrl: "./snackbar-controller.component.html",
+  animations: [
+    trigger(
+      'enterAnimation', [
+        transition(':enter', [
+          style({opacity: 0}),
+          animate('750ms', style({opacity: 1}))
+        ]),
+        transition(':leave', [
+          style({opacity: 1}),
+          animate('500ms', style({opacity: 0}))
+        ])
+      ]
+    )
+  ]
 })
-export class SnackbarControllerComponent implements OnInit {
+export class SnackbarControllerComponent implements OnChanges {
+  @Input() notifications :Array<SnackNotification>;
+  displayerInterval : any;
+  current: SnackNotification | undefined;
 
-  constructor() { }
+  ngOnChanges(changes: SimpleChanges): void {
+    this.startLoops();
+  }
 
-  ngOnInit(): void {
+  ngOnDestroy() {
+    this.clearIntervals()
+  }
+
+  clearIntervals() {
+    if(this.displayerInterval) {
+      clearInterval(this.displayerInterval);
+      this.displayerInterval = undefined;
+    }
+  }
+
+  startLoops() {
+    if (!this.displayerInterval && this.notifications.length > 0) {
+      this.current = this.notifications.shift();
+      this.displayerInterval = setInterval(() => {
+        this.current = undefined;
+        if(this.notifications.length > 0) {
+          setTimeout(() => {
+            this.current = this.notifications.shift();
+          }, 250);
+        } else {
+          this.clearIntervals()
+        }
+      }, 2750);  
+    }
+  }
+
+  deleteOne() {
+    this.current = undefined;
+    if(this.displayerInterval) {
+      clearInterval(this.displayerInterval);
+      this.displayerInterval = undefined;
+    }
+    setTimeout(() => {
+      this.startLoops();
+    }, 250);
   }
 
 }
